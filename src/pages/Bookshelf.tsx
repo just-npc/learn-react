729a47ca6976313b6da304c009144ca0c2f8e581
riiiido <121432050+just-npc@users.noreply.github.com>
2025-06-04 19:50:37 +0700

    fix book card component

--- src/pages/Bookshelf.tsx
+++ src/pages/Bookshelf.tsx
@@ -33,41 +33,8 @@ export default function Bookshelf() {
         <InputForm onSubmit={handleSubmit} />
         <SearchForm />
 
-        <BookCard>
-          <h2>Belum selesai dibaca</h2>
+        <BookCard data={books} />
 
-          <div id="incompleteBookList" data-testid="incompleteBookList">
-
-            <div className="book-item" data-bookid="123123123" data-testid="bookItem">
-              <h3 data-testid="bookItemTitle">Judul Buku 1</h3>
-              <p data-testid="bookItemAuthor">Penulis: Penulis Buku 1</p>
-              <p data-testid="bookItemYear">Tahun: 2030</p>
-              <div>
-                <button className="complete-btn" data-testid="bookItemIsCompleteButton">Selesai dibaca</button>
-                <button className="delete-btn" data-testid="bookItemDeleteButton">Hapus Buku</button>
-                <button className="edit-btn" data-testid="bookItemEditButton">Edit Buku</button>
-              </div>
-            </div>
-          </div>
-        </BookCard>
-
-        <BookCard>
-          <h2>Selesai dibaca</h2>
-
-          <div id="completeBookList" data-testid="completeBookList">
-
-            <div className="book-item" data-bookid="456456456" data-testid="bookItem">
-              <h3 data-testid="bookItemTitle">Judul Buku 2</h3>
-              <p data-testid="bookItemAuthor">Penulis: Penulis Buku 2</p>
-              <p data-testid="bookItemYear">Tahun: 2030</p>
-              <div>
-                <button className="complete-btn" data-testid="bookItemIsCompleteButton">Selesai dibaca</button>
-                <button className="delete-btn" data-testid="bookItemDeleteButton">Hapus Buku</button>
-                <button className="edit-btn" data-testid="bookItemEditButton">Edit Buku</button>
-              </div>
-            </div>
-          </div>
-        </BookCard>
       </main>
     </>
   );
